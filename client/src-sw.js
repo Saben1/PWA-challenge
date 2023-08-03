5660d89f2ea9722613428ccaabab856c84ae6221
Sabin Budhathoki <budhathokisabin303@gmail.com>
2023-08-03 15:36:36 +0800

    fixed the issues and server running

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,15 @@
-const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
-const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
-const { registerRoute } = require("workbox-routing");
-const { CacheableResponsePlugin } = require("workbox-cacheable-response");
-const { ExpirationPlugin } = require("workbox-expiration");
-const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
+const { CacheFirst } = require('workbox-strategies');
+const { registerRoute } = require('workbox-routing');
+const { CacheableResponsePlugin } = require('workbox-cacheable-response');
+const { ExpirationPlugin } = require('workbox-expiration');
+const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: "page-cache",
+  cacheName: 'page-cache',
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -20,18 +21,21 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ["/index.html", "/"],
+  urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === "navigate", pageCache);
+registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-//  Implement asset caching
+// TODO: Implement asset caching
 registerRoute(
-  ({ request }) => ["style", "worker"].includes(request.destination),
+  // define callback function that will be used to determine if a response should be cached 
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName: "asset-cache",
+    // cache storage name
+    cacheName: 'asset-cache',
     plugins: [
+      // cacheable response plugin that will be used to cache responses with status code 0 and 200 responses
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,6 +2,7 @@ import { Workbox } from 'workbox-window';
 import Editor from './editor';
 import './database';
 import '../css/style.css';
+import '../images/logo.png';
 
 const main = document.querySelector('#main');
 main.innerHTML = '';
@@ -26,8 +27,16 @@ if (typeof editor === 'undefined') {
 // Check if service workers are supported
 if ('serviceWorker' in navigator) {
   // register workbox service worker
-  const workboxSW = new Workbox('/src-sw.js');
+  const workboxSW = new Workbox('./src-sw.js');
   workboxSW.register();
 } else {
   console.error('Service workers are not supported in this browser.');
+}
+
+if (module.hot) {
+  module.hot.accept((err) => {
+    if (err) {
+      console.error('Cannot apply HMR update.', err);
+    }
+  });
 }
